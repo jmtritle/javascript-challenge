6391c3e70784fd240acfce1ba112eae1ae60f232
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-03-23 23:17:15 -0400

    populating tables

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+console.log(data);
+
+tableData.forEach(ufo => {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+    
+});
